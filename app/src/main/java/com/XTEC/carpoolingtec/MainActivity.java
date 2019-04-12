9ed6aa817e1f8e3745e0fb14bf4121db0794fcea
Gezele14 <gerardo.zeledon14@gmail.com>
2019-04-12 00:49:24 -0600

    Foto de perfil, Nombre y correo al header de la app

--- app/src/main/java/com/XTEC/carpoolingtec/MainActivity.java
+++ app/src/main/java/com/XTEC/carpoolingtec/MainActivity.java
@@ -1,5 +1,6 @@
 package com.XTEC.carpoolingtec;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +13,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private LoginButton login_button;
+    private CircleImageView circleImageView;
+    private TextView txtName, txtEmail;
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +51,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +60,98 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        View headerView = navigationView.getHeaderView(0);
+
+        //Declaracion de Variables
+        login_button = (LoginButton)findViewById(R.id.login_button);
+        txtName =  headerView.findViewById(R.id.txtName);
+        txtEmail =  headerView.findViewById(R.id.txtEmail);
+        circleImageView = (CircleImageView) headerView.findViewById(R.id.profile_image);
+
+        callbackManager = CallbackManager.Factory.create();
+        login_button.setReadPermissions(Arrays.asList("email","public_profile","user_friends"));
+        checklogin();
+
+        login_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+            }
+
+
+            @Override
+            public void onCancel() {
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken == null){
+                txtName.setText(" ");
+                txtEmail.setText(" ");
+                circleImageView.setImageResource(0);
+                Toast.makeText(MainActivity.this, "Sin inicio de Sesion", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                loaduserProfile(currentAccessToken);
+            }
+        }
+    };
+
+    private void loaduserProfile(AccessToken newAccessToken){
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                String first_name = "";
+                String last_name = "";
+                String id = "";
+                String email = "Sin Correo";
+
+                try{
+                    first_name = object.getString("first_name");
+                }catch (JSONException e){}
+                try{
+                    last_name = object.getString("last_name");
+                }catch (JSONException e){}
+                try{
+                    id = object.getString("id");
+                }catch (JSONException e){}
+                try{
+                    email = object.getString("email");
+                }catch (JSONException e){}
+
+                try {
+                    String image_url = "https://graph.facebook.com/" + id + "/picture?type=normal";
+                    txtName.setText(first_name + " " + last_name);
+                    txtEmail.setText(email);
+
+                    RequestOptions requestOptions = new RequestOptions();
+                    requestOptions.dontAnimate();
+
+                    Glide.with(MainActivity.this).load(image_url).into(circleImageView);
+                }catch (Exception e){
+                    Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields","first_name,last_name,email,id");
+        request.setParameters(parameters);
+        request.executeAsync();
+
     }
 
     @Override
@@ -97,5 +209,11 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void checklogin(){
+        if(AccessToken.getCurrentAccessToken()!= null){
+            loaduserProfile(AccessToken.getCurrentAccessToken());
+        }
     }
 }
