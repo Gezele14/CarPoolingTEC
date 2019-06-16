8ab1e18a928a3dae23af902e4f9e15137eeefcf5
CopodeNieve <juannieve1012@gmail.com>
2019-06-15 18:00:43 -0600

    PruebaUnitaria básica para Login Modificación build gradle para usar Mockito

--- app/src/test/java/com/XTEC/carpoolingtec/LoginTest.java
+++ app/src/test/java/com/XTEC/carpoolingtec/LoginTest.java
@@ -1,29 +1,40 @@
 package com.XTEC.carpoolingtec;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.validateMockitoUsage;
+import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class LoginTest {
 
+    @Mock
     private Login login1;
+
+    private Login login2;
 
     @Before
     public void setUp(){
-        login1 = new Login();
+        login2 = new Login();
     }
 
     @Test
-    public void loginNotNull(){
+    public void login(){
         assertNotNull(login1);
     }
+    @Test
+    public void registro() throws Exception {
+    }
 
-    //@Test
-    //public void registro() throws Exception {
-    //}
+    @After
+    public void tearDown(){
+    }
 
-    //@Test
-    //public void login() {
-    //}
+
 }
